4178ef5799d65c1c1212dcfe41132bbf67407bdc
Shailyy02 <111558842+Shailyy02@users.noreply.github.com>
2024-06-20 20:42:03 +0530

    added new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import UserGreeting from './UserGreeting.jsx'
-
+import Button from './Button.jsx'
+import Header from './header.jsx'
+import Footer from './Footer.jsx'
 function App(){
 
     return(
 
         <>
+          
+          <Header></Header>
+          <Footer></Footer>
+          <Button></Button>
            <UserGreeting isloggedIn={true} username="SHAILY"/>
         
         </>
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,4 +1,3 @@
-
 function Button() {
 
     const styles={
